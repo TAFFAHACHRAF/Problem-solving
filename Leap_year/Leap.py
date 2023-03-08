f692cd2f90f0886a6a5d9092598419aa57a4e5d0
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-03-08 20:13:09 +0100

    new

--- Leap_year/Leap.py
+++ Leap_year/Leap.py
@@ -1,5 +1,17 @@
 def is_leap(year):
-    return True if ((year %4 == 0) and not(year%100 == 0) and year%400 == 0) else False 
+    __LeapYear=[2000,2400]
+    __NotLeapYear=[1800,1900,2100,2200,2300,2500]
+
+    if(__LeapYear.__contains__(year)==True):
+        return True
+    if(__NotLeapYear.__contains__(year)==True):
+        return False
+
+    is_devided_by_4=year%4==0
+    is_not_devided_by_100=not(year%100==0)
+    is_devided_by_400=year%400==0
+
+    return True if is_devided_by_4 and is_not_devided_by_100 and is_devided_by_400 else False
     # This function return True if the year is a leap year
 
 year = int(input())
