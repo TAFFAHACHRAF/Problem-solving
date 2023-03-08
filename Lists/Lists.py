240eb8709a553e2fe80349ba113a5c3567b702f6
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-03-08 10:02:53 +0100

    new

---
+++ Lists/Lists.py
@@ -0,0 +1,13 @@
+import random
+
+def happy_birthday()->list:
+    years = [2001, 2002, 2003]
+    if(4==4) :
+        a=4
+    elif(8==8):
+        d=7
+    else :
+        b=7
+    return years
+
+print("Happy birthday: " + str(random.choice(happy_birthday())))
