36bedff97337e2ef5ac705f41b5c9718c76932ca
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-03-31 11:14:09 +0200

    nested list

--- nested-list/solution.py
+++ nested-list/solution.py
@@ -1,14 +1,19 @@
-n = int(input().strip())
-students = []
-for _ in range(n):
-    name = input().strip()
-    score = float(input().strip())
-    students.append((name, score))
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
 
-students = sorted(students, key=lambda x: (x[1], x[0]))
+    # Sort the list of students by score in ascending order
+    students.sort(key=lambda x: x[1])
 
+    # Find the second lowest score
+    second_lowest_score = sorted(set([x[1] for x in students]))[1]
 
-for i in range(1,len(students)):
-    print(students[i][0])
-    if(students[i][1] != students[i+1][1]):
-        break
+    # Filter the list of students to get those with the second lowest score
+    second_lowest_students = [x[0] for x in students if x[1] == second_lowest_score]
+
+    # Sort the list of second lowest students alphabetically and print their names
+    for student in sorted(second_lowest_students):
+        print(student)
